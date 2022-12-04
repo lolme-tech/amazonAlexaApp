9f22e8ac283cc36dd785918b9145a9b396d15be3
lolme <kai.eba_to11@outlook.jp>
2022-12-04 17:13:41 +0900

    added CancelAndIntentHandlerClass

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,7 @@
+###################################################
+#処理要求が発声したときにハンドラが対応
+###################################################
+
 #スキルビルダーオブジェクト
 from ask_sdk_core.skill_builder import SkillBuilder
 #陸セストを処理して応答を返すリクエストハンドラー
@@ -23,4 +27,48 @@ class LaunchRequestHandler(AbstractRequestHandler):
         handler_input.response_builder.speak(speech_text).set_card(
             SimpleCard("ハローワールド", speech_text)).set_should_end_session(
             False)
+        return handler_input.response_builder.response
+
+#HelloWorldIntentという院展とリクエストを受け取ったときに呼び出されるハンドラを設定する
+class HelloWorldIntentHandler(AbstractRequestHandler):
+    #処理要求がIntentRequestの時かつインテント名がHelloWorldIntentの時Trueを返す
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("HelloWorldIntent")(handler_input)
+
+    #「こんにちは」という応答生成
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "こんにちは"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("ハローワールド", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
+
+class HelpIntentHandler(AbstractRequestHandler):
+    #処理要求がIntentRequestの時かつAMAZON.HelpIntentの時Trueを返す
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "こんにちは。と言ってみてください。"
+        #.ask(speech_text)でユーザのマイクがオンになりユーザの応答待ち
+        handler_input.response_builder.speak(speech_text).ask(speech_text).set_card(
+            SimpleCard("ハローワールド", speech_text))
+        return handler_input.response_builder.response
+
+class CancelAndStopIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.CancelIntent")(handler_input) or is_intent_name("AMAZON.StopIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "さようなら"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("ハローワールド", speech_text)).set_should_end_session(True)
         return handler_input.response_builder.response
