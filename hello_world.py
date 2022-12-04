9f1e1b3a40dcc4d3e5c2872d509be54881b35153
かい <63903850+lolme-tech@users.noreply.github.com>
2022-12-04 16:57:39 +0900

    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,16 @@
+#スキルビルダーオブジェクト
 from ask_sdk_core.skill_builder import SkillBuilder
+#陸セストを処理して応答を返すリクエストハンドラー
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
 
+#入力されたリクエスト処置とカスタム応答の生成
 sb = SkillBuilder()
 
+#受け取ったリクエストがLaunchRequestの時、can_handle関数の戻り値はTrue
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
